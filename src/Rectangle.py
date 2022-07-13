e0f36c30383d611898d0682d6950bbb19783fdc4
Anton Karavashkin <anton.karavashkin@simbirsoft.com>
2022-07-13 13:45:41 +0400

    otus web qa 2022-06 lesson3 oop+pytest_2

--- src/Rectangle.py
+++ src/Rectangle.py
@@ -4,7 +4,7 @@ from src.Figure import Figure
 class Rectangle(Figure):
     def __init__(self, side_a=None, side_b=None):
         if None in [side_a, side_b]:
-            raise ValueError('Треугольник задается тремя сторонами')
+            raise ValueError('Прямоугольник задается двумя сторонами')
         else:
             self.name = "Прямоугольник"
             self.side_a = side_a
--- tests/test_rectangle.py
+++ tests/test_rectangle.py
@@ -3,7 +3,7 @@ from data.geom_data import BAG_RECTANGLE, RECTANGLE_NAME, EXPECTED_RECTANGLE_ARE
 import pytest
 
 
-def test_impossible_triangle_creation():
+def test_impossible_rectangle_creation():
     """
     Тест создания не правильного прямоугольника
     """
@@ -11,14 +11,14 @@ def test_impossible_triangle_creation():
         Rectangle(*BAG_RECTANGLE)
 
 
-def test_triangle_creation(rectangle_creation):
+def test_rectangle_creation(rectangle_creation):
     """
     Тест проверяет успешное создание экземпляра класса Rectangle
     """
     assert type(rectangle_creation) == Rectangle
 
 
-def test_triangle_name(rectangle_creation):
+def test_rectangle_name(rectangle_creation):
     """
     Тест проверяет атрибут name
     """
@@ -26,21 +26,21 @@ def test_triangle_name(rectangle_creation):
         f"Ожидалось имя {RECTANGLE_NAME}, пришло {rectangle_creation.name}"
 
 
-def test_triangle_add_area(rectangle_creation, square_creation):
+def test_rectangle_add_area(rectangle_creation, square_creation):
     added_area = rectangle_creation.add_area(square_creation)
     assert added_area == square_creation.add_area(rectangle_creation), "Передена не фигура"
 
 
-def test_triangle_wrong_figure(rectangle_creation):
+def test_rectangle_wrong_figure(rectangle_creation):
     with pytest.raises(ValueError):
         rectangle_creation.add_area(10)
 
 
-def test_triangle_area(rectangle_creation):
+def test_rectangle_area(rectangle_creation):
     assert rectangle_creation.area == EXPECTED_RECTANGLE_AREA, \
         f"Площади не совпадают, ожидалось {EXPECTED_RECTANGLE_AREA}"
 
 
-def test_triangle_perimeter(rectangle_creation):
+def test_rectangle_perimeter(rectangle_creation):
     assert rectangle_creation.perimeter == EXPECTD_RECTANGLE_PERIMETER, \
         f"Периметр не совпадает, ожидался {EXPECTD_RECTANGLE_PERIMETER}"
