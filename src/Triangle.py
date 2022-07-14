f6d9e3587cbceb65d11ffc3725fbb7b333c6d134
antonkaravashkin <anton.karavashkin@simbirsoft.com>
2022-07-14 19:53:42 +0400

    Update Triangle.py

--- src/Triangle.py
+++ src/Triangle.py
@@ -2,16 +2,15 @@ from src.Figure import Figure
 import math
 
 
-class Triangle(Figure):
-    def __new__(cls, *args):
-        if None in args:
+class Triangle(Figure):    
+    def __init__(self, side1=None, side2=None, side3=None):     
+        if None in [side1, side2, side3]:
             raise ValueError('Треугольник задается тремя сторонами')
-    
-    def __init__(self, side1=None, side2=None, side3=None):
-        self.name = "Треугольник"
-        self.side1 = side1
-        self.side2 = side2
-        self.side3 = side3
+        else:
+            self.name = "Треугольник"
+            self.side1 = side1
+            self.side2 = side2
+            self.side3 = side3
     
     @property
     def area(self):
