c1e1633cb31a2f10bcde650f0c996c196cae0d8e
Anton Karavashkin <anton.karavashkin@simbirsoft.com>
2022-07-18 19:36:29 +0400

    read-from-file-otus-qa-web-python-fixed-order

--- lesson_5_test_data/read_from_file.py
+++ lesson_5_test_data/read_from_file.py
@@ -1,34 +1,28 @@
 import json
 import csv
-from collections import OrderedDict
 
 
-def func_tools(csv_listk, step):
-    for i in range(0, len(csv_listk), step):
-        yield csv_listk[i:i + step]
-
-
-def unpacking(lst):
-    for i in lst:
-        return i
+def split_list(alist, wanted_parts=1):
+    length = len(alist)
+    return [alist[i * length // wanted_parts: (i + 1) * length // wanted_parts]
+            for i in range(wanted_parts)]
 
 
 with open("books.csv", "r") as csv_b:
     book_list = []
     reader = csv.DictReader(csv_b)
-    for zero in reader:
-        res = {key: zero[key] for key in zero.keys()}
-        book_list.append(res)
+    for books in reader:
+        new_books_dict = {key: books[key] for key in books.keys()}
+        book_list.append(new_books_dict)
 
 with open("users.json", "r") as users:
     updated_users = []
-    users_table = {"name", "gender", "address", "age", "books"}
+    users_table = ("name", "gender", "address", "age", "books")
     json_string = json.load(users)
-    peso = func_tools(book_list, 8)
-    for item in json_string:
-        unpacked = unpacking(peso)
-        item.setdefault("books", unpacked)
-        generated_json = OrderedDict((key, item[key]) for key in users_table if key in item)
+    splited_books = split_list(alist=book_list, wanted_parts=28)
+    for item, value in zip(json_string, splited_books):
+        item["books"] = value
+        generated_json = ({key: item[key] for key in users_table})
         updated_users.append(generated_json)
         with open("result.json", "w") as ref:
             json.dump(updated_users, ref, indent=4)
