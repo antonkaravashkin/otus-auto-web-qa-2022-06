68916a0f515c4430d8b278ef2c2619d6dbfc4038
Anton Karavashkin <anton.karavashkin@simbirsoft.com>
2022-07-21 12:23:27 +0400

    read-from-file-otus-qa-web-python-fixed-generated-json-order

--- lesson_5_test_data/read_from_file.py
+++ lesson_5_test_data/read_from_file.py
@@ -8,21 +8,15 @@ def split_list(alist, wanted_parts=1):
             for i in range(wanted_parts)]
 
 
-with open("books.csv", "r") as csv_b:
-    book_list = []
-    reader = csv.DictReader(csv_b)
-    for books in reader:
-        new_books_dict = {key: books[key] for key in books.keys()}
-        book_list.append(new_books_dict)
-
-with open("users.json", "r") as users:
-    updated_users = []
-    users_table = ("name", "gender", "address", "age", "books")
-    json_string = json.load(users)
-    splited_books = split_list(alist=book_list, wanted_parts=28)
-    for item, value in zip(json_string, splited_books):
+def merging_users_books():
+    result_json = []
+    with open("books.csv", "r") as csv_b, open("users.json", "r") as users:
+        books = list(csv.DictReader(csv_b))
+        users_json = json.load(users)
+        splitted_books = split_list(alist=books, wanted_parts=len(users_json))
+    for item, value in zip(users_json, splitted_books):
         item["books"] = value
-        generated_json = ({key: item[key] for key in users_table})
-        updated_users.append(generated_json)
-        with open("result.json", "w") as ref:
-            json.dump(updated_users, ref, indent=4)
+        generated_user = {key: item[key] for key in ("name", "gender", "address", "age", "books")}
+        result_json.append(generated_user)
+    with open("result.json", "w") as result:
+        json.dump(result_json, result, indent=4)
